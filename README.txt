aaa871d3dfb666c00a7ff57679d006e56654ac12
Simon Wessling <simon.wessling@tuhh.de>
2017-06-25 00:35:36 +0200

    initial setup

---
+++ README.txt
@@ -0,0 +1,5 @@
+Integration:
+- Adapt page title
+- Adapt dependencies:
+	 jQuery-Path
+	 literallycanvas
---
+++ gw_sketch.js
@@ -0,0 +1,7 @@
+var lcConfig = {
+	onInit:				function() {console.log("init")},
+    imageURLPrefix: 	'libs/literallycanvas-0.4.14/img',
+    toolbarPosition: 	'bottom',
+    defaultStrokeWidth: 2,
+    strokeWidths: 		[1, 2, 3, 5, 30]
+};
