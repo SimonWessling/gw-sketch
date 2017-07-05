1f11d7498efe85efda6652b9cf4a3f6f05bce49e
Simon Wessling <simon.wessling@tuhh.de>
2017-07-05 22:49:25 +0200

    Refactored to implement revealing module pattern

--- gw_sketch.js
+++ gw_sketch.js
@@ -1,128 +1,156 @@
-// TODO implement revealing module pattern
-var localStorageSupported = typeof(Storage) == "undefined" ? false : true;
-var isStandalone = true;
-var lc = null;
-var currentSnapshot = null;
-var currentSnapshotName = null;
-
-var lcConfig = {
-//	onInit:				
-    imageURLPrefix: 	'libs/literallycanvas-0.4.14/img',
-    backgroundColor:	'#fff',
-    toolbarPosition: 	'bottom',
-    tools:				[
-    						LC.tools.Pencil,
-    						LC.tools.Eraser,
-    						LC.tools.Line,
-    						LC.tools.Rectangle,
-    						LC.tools.Text,    						
-    					],
-    defaultStrokeWidth: 3,
-    strokeWidths: 		[1, 2, 3, 5]
-};
-
-
-$(function() {
-	lc = LC.init(
-			document.getElementById('lc'),
-			lcConfig
-	);
-	if (isStandalone) {
-		initExtraControls();
-	}
-});
-
-function initExtraControls() {
-	$('#controls').append(
-		 '<button type="button" class="controls" onclick="load()">Laden</button>'
-		+'<button type="button" class="controls" onclick="save()">Speichern</button>'
-		+'<button type="button" class="controls" onclick="save(true)">Speichern unter</button>'
-		+'<button type="button" class="controls" onclick="exportAs(\'png\')">Exportieren</button>'
-	);
-}
-
-/*
- * Wraps the logic of persisting snapshots by getting a file name if necessary
- * and then persisting the current state under that name.
- */
-function save(getNewName = false) {
-	if (getNewName || !currentSnapshotName) { 
-		currentSnapshotName = prompt('Speichern als:'); // TODO
-		// TODO user feedback: Name der aktuellen Skizze anzeigen
-		if (!currentSnapshotName)
-			return;
-	}
-	saveSnapshotUnderName(currentSnapshotName);
-	// TODO user feedback
-}
-
-/*
- * Wraps the logic of loading a sketch by getting the name of the sketch
- * and loading it.
- */
-function load() {
-	// TODO alten snapshot speichern?
-	currentSnapshotName = prompt('Name der zu ladenden Skizze:');
-	if (currentSnapshotName) {
-		loadSnapshotByName(currentSnapshotName);	
-	}
-}
-
-function exportAs(format) {
-	if (format == 'png') {
-		var image = lc.getImage();
-		if(image) {
-			return image.toDataURL();
-		} else {
-			//TODO warn: empty image
-			return null;
-		}
-	}
-	else if (format == 'svg'){
-
-	}
-	else {
-		// Format nicht unterstützt
-	}
-}
-
-/*
- * Persists the current state to local storage under the given name.
- */
-function saveSnapshotUnderName(snapshotName)
+var sketchModule = (function () 
 {
-	if (!snapshotName) {
-		return;
-	}
-	currentSnapshot = lc.getSnapshot();
+	/******************************** Configurable options ********************************************/
+	var isStandalone = true;
+	var lcConfig = {
+//			onInit:				
+					imageURLPrefix: 	'libs/literallycanvas-0.4.14/img',
+					backgroundColor:	'#fff',
+					toolbarPosition: 	'bottom',
+					tools:				[
+					      				 LC.tools.Pencil,
+					      				 LC.tools.Eraser,
+					      				 LC.tools.Line,
+					      				 LC.tools.Rectangle,
+					      				 LC.tools.Text,    						
+					      				 ],
+					      				 defaultStrokeWidth: 3,
+					      				 strokeWidths: 		[1, 2, 3, 5]
+	};
 	
-	if (localStorageSupported) {
-			localStorage.setItem(snapshotName, JSON.stringify(currentSnapshot));
-	}
-	else {
-		// TODO error handling no storage
-	}
-}
-
-/*
- * Loads a persisted snapshot (as identified by the name) and renders it.
- */
-function loadSnapshotByName(snapshotName) 
-{
-	if (!snapshotName) {
-		return;
-	}
-	if (localStorageSupported) {
-		var snapshot = JSON.parse(localStorage.getItem(snapshotName));
-		if (snapshot) {
-			lc.loadSnapshot(snapshot); // render snapshot
-			currentSnapshot = snapshot;
+	/******************************** "Private" variables ********************************************/
+	
+	var moduleName = 'sketchModule'; // needs to correspond to the name of the variable this module is held in
+	var localStorageSupported = typeof(Storage) == "undefined" ? false : true;
+	var currentSnapshot = null;
+	var currentSnapshotName = null;
+	var lc = null;
+	
+	/******************************** "Private" functions ********************************************/
+	
+	/*
+	 * Displays extra control buttons for the basic functions.
+	 */
+	initExtraControls = function()
+	{
+		$('#controls').append(
+			 '<button type="button" class="controls" onclick="' + moduleName + '.load()">Laden</button>'
+			+'<button type="button" class="controls" onclick="' + moduleName + '.save()">Speichern</button>'
+			+'<button type="button" class="controls" onclick="' + moduleName + '.save(true)">Speichern unter</button>'
+			+'<button type="button" class="controls" onclick="' + moduleName + '.exportAs(\'png\')">Exportieren</button>'
+		);
+	},
+	
+	
+	/*
+	 * Persists the current state to local storage under the given name.
+	 */
+	saveSnapshotUnderName = function(snapshotName)
+	{
+		if (!snapshotName) {
+			return;
+		}
+		currentSnapshot = lc.getSnapshot();
+		
+		if (localStorageSupported) {
+				localStorage.setItem(snapshotName, JSON.stringify(currentSnapshot));
 		}
 		else {
-			// TODO
-			alert("Keine Skizze mit diesem Namen gefunden!");
-		}	
-	} else {
-		// TODO error handling no storage
+			// TODO error handling no storage
+		}
+	};
+
+	/*
+	 * Loads a persisted snapshot (as identified by the name) and renders it.
+	 */
+	loadSnapshotByName = function(snapshotName) 
+	{
+		if (!snapshotName) {
+			return;
+		}
+		if (localStorageSupported) {
+			var snapshot = JSON.parse(localStorage.getItem(snapshotName));
+			if (snapshot) {
+				lc.loadSnapshot(snapshot); // render snapshot
+				currentSnapshot = snapshot;
+			}
+			else {
+				// TODO
+				alert("Keine Skizze mit diesem Namen gefunden!");
+			}	
+		} else {
+			// TODO error handling no storage
+		}
 	}
-}
+	
+	/******************************** "Public" API ********************************************/
+	return {
+		
+		/*
+		 * Initializes the literallyCanvas object and the extra buttons (if standalone)
+		 */
+		init: function() 
+		{
+			lc = LC.init(
+					document.getElementById('lc'),
+					lcConfig
+			);
+			if (isStandalone) {
+				initExtraControls();
+			}
+		},
+
+		/*
+		 * Wraps the logic of persisting snapshots by getting a file name if necessary
+		 * and then persisting the current state under that name.
+		 */
+		save : function(getNewName = false) {
+			if (getNewName || !currentSnapshotName) { 
+				currentSnapshotName = prompt('Speichern als:'); // TODO
+				// TODO user feedback: Name der aktuellen Skizze anzeigen
+				if (!currentSnapshotName)
+					return;
+			}
+			saveSnapshotUnderName(currentSnapshotName);
+			// TODO user feedback
+		},
+	
+		/*
+		 * Wraps the logic of loading a sketch by getting the name of the sketch
+		 * and loading it.
+		 */
+		load : function() {
+			// TODO alten snapshot speichern?
+			currentSnapshotName = prompt('Name der zu ladenden Skizze:');
+			if (currentSnapshotName) {
+				loadSnapshotByName(currentSnapshotName);	
+			}
+		},
+	
+		/*
+		 * Returns the current state as either png or svg
+		 */
+		exportAs : function(format) {
+			if (format == 'png') {
+				var image = lc.getImage();
+				if(image) {
+					return image.toDataURL();
+				} else {
+					//TODO warn: empty image
+					return null;
+				}
+			}
+			else if (format == 'svg'){
+	
+			}
+			else {
+				// Format nicht unterstützt
+			}
+		}
+	};
+})();
+
+$(function() {
+	sketchModule.init();
+});
+
